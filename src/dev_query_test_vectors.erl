86a4690cf5635c9273ac0030dba155b8847ff45d
Rani Elhusseini <charmful0x@gmail.com>
2026-04-20 20:09:02 +0200

    fix: stale start-offset usage

--- src/dev_query_test_vectors.erl
+++ src/dev_query_test_vectors.erl
@@ -740,7 +740,7 @@ transactions_query_ids_preserve_arweave_tx_id_test_parallel() ->
     {ok, _Node, Opts} = test_env_with_blocks(1892487, 1892487),
     ID = <<"mT7pIQx9ORnemXoIzWmKwymiZJxtOSvzxm3P44M9C1A">>,
     ?assertMatch(
-        {ok, #{ <<"start-offset">> := _ }},
+        {ok, #{ <<"offset">> := _ }},
         hb_store_arweave:read_offset(hb_store_arweave:store_from_opts(Opts), ID)
     ),
     ?assertMatch(
@@ -772,9 +772,9 @@ transactions_query_cursor_by_offset_test_parallel() ->
     EarlierID = <<"xBpOR2KOjYEgv5HmddMlAgYa-yMvfEVl-0XzRIfm2uY">>,
     LaterID = <<"HVr7EpRhlPkbwdnoXKHf25p7BPa0qJOs6C7XueLthA0">>,
     StoreOpts = hb_store_arweave:store_from_opts(Opts),
-    {ok, #{ <<"start-offset">> := EarlierOffset }} =
+    {ok, #{ <<"offset">> := EarlierOffset }} =
         hb_store_arweave:read_offset(StoreOpts, EarlierID),
-    {ok, #{ <<"start-offset">> := LaterOffset }} =
+    {ok, #{ <<"offset">> := LaterOffset }} =
         hb_store_arweave:read_offset(StoreOpts, LaterID),
     Query =
         <<"""
